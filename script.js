294d4c9dc53e440702b69b597d9c8d29ede3ad18
Micaela Kloster <micaela.klosterquintana@gmail.com>
2022-09-08 18:44:18 -0300

    Changes

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 //? CREACIÓN DE LA FUNCIÓN PRINCIPAL 
 $(function () {
     function c() {
-    p();
-    var e = h();
-    var r = 0;
-    var u = false;
-    l.empty();
-    while (!u) {
-    if (s[r] == e[0].weekday) {
-    u = true;
-    } else {
-    l.append('<div class="blank"></div>');
-    r++;
-    }
+        p();
+        var e = h();
+        var r = 0;
+        var u = false;
+        l.empty();
+        while (!u) {
+            if (s[r] == e[0].weekday) {
+                u = true;
+            } else {
+                l.append('<div class="blank"></div>');
+                r++;
+            }
     }
     for (var c = 0; c < 42 - r; c++) {
-    if (c >= e.length) {
-    l.append('<div class="blank"></div>');
-    } else {
-    var v = e[c].day;
-    var m = g(new Date(t, n - 1, v)) ? '<div class="today">' : "<div>";
-    l.append(m + "" + v + "</div>");
-    }
+        if (c >= e.length) {
+            l.append('<div class="blank"></div>');
+        }else {
+            var v = e[c].day;
+            var m = g(new Date(t, n - 1, v)) ? '<div class="today">' : "<div>";
+            l.append(m + "" + v + "</div>");
+        }
     }
     var y = o[n - 1];
     a.css("background-color", y)
